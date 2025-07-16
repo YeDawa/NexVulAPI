2051db0b96efe202ec8c4e11beb580fe5d9c842f
Kremilly <contact@kremilly.com>
2025-07-16 00:55:32 -0300

    feat: add CORS middleware for enhanced API security and flexibility

--- main.go
+++ main.go
@@ -6,12 +6,31 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 
 	"httpshield/controllers/engine"
 )
 
 func main() {
 	e := echo.New()
+
+	// Enable CORS with credentials support
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:8080", "http://localhost:3001", "http://127.0.0.1:3000", "http://127.0.0.1:8080", "https://httpshield.net", "http://localhost:5173"},
+		AllowMethods:     []string{echo.GET, echo.POST, echo.PUT, echo.DELETE, echo.OPTIONS, echo.HEAD, echo.PATCH},
+		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization, "Cookie", "Set-Cookie"},
+		ExposeHeaders:    []string{"Set-Cookie", "Authorization"},
+		AllowCredentials: true,
+		MaxAge:           86400,
+	}))
+
+	// Add middleware to set common headers
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			c.Response().Header().Set("Access-Control-Allow-Credentials", "true")
+			return next(c)
+		}
+	})
 
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found, trying to use system environment variables")
