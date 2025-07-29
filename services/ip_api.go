3fce75ddb084df2402ac21014418a213afe108c9
Kremilly <contact@kremilly.com>
2025-07-29 12:53:57 -0300

    feat: refactor GetIPInfo function to handle IP address retrieval and response formatting

--- services/ip_api.go
+++ services/ip_api.go
@@ -5,28 +5,31 @@ import (
 	"net/http"
 
 	"nexvul/utils"
+
+	"github.com/labstack/echo/v4"
 )
 
-func GetIPInfo(ip string) string {
+func GetIPInfo(c echo.Context) error {
+	ip := c.QueryParam("ip")
+	if ip == "" {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "IP address is required"})
+	}
+
 	url := fmt.Sprintf("http://ip-api.com/json/%s", ip)
 	resp, err := http.Get(url)
 	if err != nil {
-		return fmt.Sprintf("Error fetching IP info: %v", err)
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to fetch IP information"})
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Sprintf("Error fetching IP info: %v", err)
+		return c.JSON(resp.StatusCode, map[string]string{"error": "Failed to fetch IP information"})
 	}
 
 	var ipInfo map[string]interface{}
 	if err := utils.DecodeJSON(resp.Body, &ipInfo); err != nil {
-		return fmt.Sprintf("Error decoding IP info: %v", err)
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to decode IP information"})
 	}
 
-	jsonBytes, err := utils.EncodeJSON(ipInfo)
-	if err != nil {
-		return fmt.Sprintf("Error encoding IP info: %v", err)
-	}
-	return string(jsonBytes)
+	return c.JSON(http.StatusOK, ipInfo)
 }
